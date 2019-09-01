1e2e69de943fc91c92864f9f4bc6727bac3b9b3d
Satheesh Valluru <vallurusateesh2009@gmail.com>
2019-09-01 11:32:40 +0530

    emails

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config import Config
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -15,6 +16,8 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
+
+mail = Mail(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
